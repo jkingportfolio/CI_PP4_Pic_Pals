3c5bc008f45a741585ebfbd548cdc5f3e04599d6
jkingportfolio <mycodejourney3@gmail.com>
2022-12-10 16:54:57 +0000

    Add: Basis of log in form,html,view and url

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
-from account import views
+from . import views
 from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.index, name='login'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,30 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginDetails
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginDetails(request.Post)
+        if form.is_valid():
+            clearform = form.cleaned_data
+            user = authenticate(request,
+            username = clearform['username'],
+            password = clearnform['password'])
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse(('Login authenticated'))
+            else:
+                return HttpResponse(('Account disabled'))
+        else:
+            return(HttpResponse('Invalid login'))
+    else:
+        form = LoginDetais()
+    return render(request, 'account/login.html', {'form': form})
+
+        
