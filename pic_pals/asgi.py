09f86ca8de20f64928ff97dd9656e06801519fbd
jkingportfolio <mycodejourney3@gmail.com>
2022-12-10 11:37:17 +0000

    Add: Django project and app

---
+++ pic_pals/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pic_pals project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pic_pals.settings')
+
+application = get_asgi_application()
