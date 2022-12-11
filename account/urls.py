7b8dbe4a8021572faca5534d3b04de3d6bc8244d
jkingportfolio <mycodejourney3@gmail.com>
2022-12-11 12:55:28 +0000

    Add: django authentication framework login html

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # Using djangos prefab authentication views
+    # https://docs.djangoproject.com/en/4.1/topics/auth/default/#all-authentication-views
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
