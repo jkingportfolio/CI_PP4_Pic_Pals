96a666d05a6e864574b33f86672875dc33a81f79
jkingportfolio <mycodejourney3@gmail.com>
2022-12-11 12:32:37 +0000

    Fix: syntax errors preventing login test logs

--- account/views.py
+++ account/views.py
@@ -7,20 +7,21 @@ from .forms import LoginDetails
 # def index(request):
 #     return render(request, 'index.html')
 
+
 def user_login(request):
     if request.method == 'POST':
-        form = LoginDetails(request.Post)
+        form = LoginDetails(request.POST)
         if form.is_valid():
             clearform = form.cleaned_data
-            user = authenticate(request, username=clearform['username'], password=clearnform['password'])
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponse(('Login authenticated'))
+            user = authenticate(request, username=clearform['username'], password=clearform['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse(('Login authenticated'))
+                else:
+                    return HttpResponse(('Account disabled'))
             else:
-                return HttpResponse(('Account disabled'))
-        else:
-            return (HttpResponse('Invalid login'))
+                return (HttpResponse('Invalid login'))
     else:
         form = LoginDetails()
     return render(request, 'account/login.html', {'form': form})
