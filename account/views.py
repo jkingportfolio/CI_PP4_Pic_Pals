65dd480cd15b63b6831c6df884a1c568445251e8
jkingportfolio <mycodejourney3@gmail.com>
2022-12-12 15:00:14 +0000

    Add: Register new user view

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginDetails
+from .forms import LoginDetails, Registration
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -31,3 +31,17 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': dashboard})
+
+
+# User registration view
+def register(request):
+    if request.method == 'POST':
+        user_form = Registration(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_success.html', {'new_user': new_user})
+        else:
+            user_form = Registration()
+        return render(request, 'account/register.html', {'user_form': user_form})
