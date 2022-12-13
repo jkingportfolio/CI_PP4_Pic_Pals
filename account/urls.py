bc7a9e5296b4e863b4069dacf1fb705edea0d3f5
jkingportfolio <mycodejourney3@gmail.com>
2022-12-13 11:27:09 +0000

    Add: Password change html pages and url pattern

--- account/urls.py
+++ account/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
 ]
