add394460b8a61fa6d67941e584050012afd372d
jkingportfolio <mycodejourney3@gmail.com>
2022-12-13 11:34:18 +0000

    Update: url paths _ with -

--- account/urls.py
+++ account/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
 ]
