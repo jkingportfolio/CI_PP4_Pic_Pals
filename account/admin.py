a9d02f936e04acaa4537b7ddad70e42ad0834fd1
jkingportfolio <mycodejourney3@gmail.com>
2022-12-13 14:43:09 +0000

    Add: Register Profile model to admin site

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ['user','dob','profile_pic']
+    raw_id_fields = ['user']
