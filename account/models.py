3580692f058c931d8a682da02f67352673d0ebf5
jkingportfolio <mycodejourney3@gmail.com>
2022-12-13 14:34:31 +0000

    Add: Profile model for additional user info

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django import settings
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    dob = models.DateField(blank=True, null=True)
+    profile_pic = CloudinaryField(blank=True, default='default_profile_pic.jpeg')
+
+    def __str__(self):
+        return f'Profile of {self.user.username}'
