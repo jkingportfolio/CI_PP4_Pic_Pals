7fb0d1c3f6a7a1b903b8d68ff28a85509bc1d0c9
jkingportfolio <mycodejourney3@gmail.com>
2022-12-14 11:38:55 +0000

    Update: Register will create a new Profile object

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginDetails, Registration
+from .forms import LoginDetails, Registration, Profile
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -41,6 +41,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(request, 'account/register_success.html', {'new_user': new_user})
     else:
         user_form = Registration()
