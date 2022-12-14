395aaf4fc524c1bc5e00bf8824d67b85ef0e1496
jkingportfolio <mycodejourney3@gmail.com>
2022-12-14 11:56:55 +0000

    Add: Edit profile URL pattern to urls.py

--- account/urls.py
+++ account/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('password-reset-done/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_done'),
     path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('edit-profile/', views.Pedit, name='edit_profile'),
 ]
