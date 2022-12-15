e4d38b829e613b9cc4498ff92ebe8bab289f3448
jkingportfolio <mycodejourney3@gmail.com>
2022-12-15 11:20:51 +0000

    Add: User model import

--- post/models.py
+++ post/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
