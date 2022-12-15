6ecb3a6790f03013cd46c88bff49a3f83d14b511
jkingportfolio <mycodejourney3@gmail.com>
2022-12-15 11:27:09 +0000

    Add: Register Post in admin.py

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'post_image', 'created_date')
+    list_filter = ('created_date')
