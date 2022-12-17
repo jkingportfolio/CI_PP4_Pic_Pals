ecc3647cee010e41de431a2e22abf9e22164d31f
jkingportfolio <mycodejourney3@gmail.com>
2022-12-17 10:43:14 +0000

    Update: Profile model fields

--- account/models.py
+++ account/models.py
@@ -8,6 +8,8 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     dob = models.DateField(blank=True, null=True)
     profile_pic = CloudinaryField(blank=True, default='default_profile_pic.jpeg')
+    bio = models.CharField(max_length=5000, blank=True)
+    # location with google maps api to potentially be future implemented
 
     def __str__(self):
         return f'Profile of {self.user.username}'
