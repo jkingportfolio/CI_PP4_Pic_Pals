341e41a50fe74fbff847f6452b9569436ce93f3f
jkingportfolio <mycodejourney3@gmail.com>
2022-12-17 11:52:02 +0000

    Fix: Issue with create_post link on dashboard

--- post/urls.py
+++ post/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = 'post'
+app_name = 'posts'
 
 urlpatterns = [
     path('create-post/', views.create_post, name='create_post'),
--- post/views.py
+++ post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .forms import PostImageForm
 from django.contrib.auth.decorators import login_required
 from .models import Post
