c443d4c884d5128cb754ddb6aa7d5875fd0e826b
jkingportfolio <mycodejourney3@gmail.com>
2022-12-17 13:53:04 +0000

    Fix: Broken db, delete all migrations and migrate

--- post/forms.py
+++ post/forms.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostImageForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'description', 'post_image']
+        fields = ['caption', 'post_image']
--- post/models.py
+++ post/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django.conf import settings
-from django.utils.text import slugify
 from django.urls import reverse
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+import uuid
 
 # Create your models here.
 
 class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='user_post', on_delete=models.CASCADE)
-    description = models.CharField(max_length=10000, blank=True)
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, blank=True)
+    caption = models.CharField(max_length=10000, blank=True)
     post_image = CloudinaryField('image', default='placeholder')
     created_date = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='post_like', blank=True)
@@ -32,5 +31,5 @@ class Post(models.Model):
 
     # Define canonical URL
     def get_absolute_url(self):
-        return reverse('post:user_posts', args=[self.slug])
+        return reverse('post:post_detail', args=[self.slug])
 
