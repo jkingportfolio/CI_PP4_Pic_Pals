a48e73e8ac2d4c3a4bd04ff9f8324bc0962348ec
jkingportfolio <mycodejourney3@gmail.com>
2022-12-18 20:30:04 +0000

    Update: User to Post for all users

--- account/views.py
+++ account/views.py
@@ -74,7 +74,7 @@ def edit_profile(request):
 
 @login_required
 def site_users(request):
-    users = User.objects.filter(is_active=True)
+    users = Profile.objects.filter(is_active=True)
     return render(request, 'account/user/user_list.html', {'section':'people', 'users': users})
 
 @login_required
