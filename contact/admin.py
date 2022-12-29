b99499ac587288ae1e80c969445e9935cb846e29
jkingportfolio <mycodejourney3@gmail.com>
2022-12-29 18:17:57 +0000

    Add: Register ContactAdmin in admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Admin Class for Contact Model
+    """
+    list_display = (
+        'message_date'
+        'name',
+        'reason',
+        'message',
+        'email'
+        )
