962486901e122f9deb645720e0123659e4b4e161
jkingportfolio <mycodejourney3@gmail.com>
2022-12-29 18:11:35 +0000

    Add: Contact app and model

---
+++ contact/models.py
@@ -0,0 +1,46 @@
+
+from django.db import models
+
+
+class Contact(models.Model):
+    """
+    A class for the contact model
+    """
+    class Reason(models.TextChoices):
+        """
+        Contact reason options
+        """
+        LOGIN = "1", "Login issue"
+        REPORT_POST = "2", "Report a post"
+        GENERAL = "3", "General Enquiry"
+        # (...)
+
+    reason = models.CharField(
+        max_length=2,
+        choices=Reason.choices,
+        default=Reason.GENERAL
+    )
+    name = models.CharField(
+        max_length=50
+        )
+    email = models.EmailField(
+        max_length=70
+        )
+    message_body = models.TextField(
+        max_length=500
+        )
+    message_date = models.DateTimeField(
+        auto_now=True
+        )
+
+
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+
+    def __str__(self):
+        """
+        Returns contact name and date as a string
+        """
+        return self.name
+        return f'Message from {self.name} on {self.message_date}'
