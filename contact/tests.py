388b029fcd63698fde62652742b02a14b20be9f3
jkingportfolio <mycodejourney3@gmail.com>
2023-01-01 15:18:48 +0000

    Add: Basis of testing files for all apps

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,30 @@
+"""
+A module for testing views of the contact app
+"""
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.test import TestCase
+# Internal
+from .models import Contact
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    """
+    A class for testing contact views
+    """
+    def setUp(self):
+        # Setup run before every test method.
+        pass
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
+    def test_something_that_will_pass(self):
+        self.assertFalse(False)
+
+    def test_something_that_will_fail(self):
+        self.assertTrue(False)
+
