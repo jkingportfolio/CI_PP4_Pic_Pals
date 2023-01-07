903f74a3687125f8d4f5f0c899e0cb5273861f2f
jkingportfolio <mycodejourney3@gmail.com>
2023-01-07 16:44:54 +0000

    Add: Contact form unit test

---
+++ contact/test_forms.py
@@ -0,0 +1,22 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.test import TestCase
+from django.contrib.auth.models import User
+# Internal:
+from .models import Contact
+from .forms import ContactForm
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class TestContactForm(TestCase):
+    """
+    A class to test ContactForm
+    """
+    def test_empty_form(self):
+        """
+        To test empty form
+        """
+        form = ContactForm()
+        self.assertIn("name", form.fields)
+        self.assertIn("email", form.fields)
