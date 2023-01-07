4d139a3c8dc7e6fe21b9f61af849d86fbaa12e70
jkingportfolio <mycodejourney3@gmail.com>
2023-01-07 12:43:12 +0000

    Add: Contact form unit test

--- post/views.py
+++ post/views.py
@@ -56,7 +56,7 @@ def edit_post(request, id):
             post.save()
             messages.success(request, 'Post caption updated successfully.')
             # The below works but causes issues, page  rendered is correct but not url
-            # return render(request, 'post/post_detail.html', {'post': post})
+            return redirect(reverse('post_detail', args={post.id}))
         else:
             messages.error(request, 'Error updating post caption.')
             return render(request, 'post/post_detail.html', context)
