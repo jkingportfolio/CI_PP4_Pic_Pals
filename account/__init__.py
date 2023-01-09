41ba12c7b329ca81faf2bab29752c3a7d1a7e71e
jkingportfolio <mycodejourney3@gmail.com>
2023-01-09 14:37:53 +0000

    Add: Log in and out signals

---
+++ account/__init__.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.dispatch import receiver
+from django.contrib import messages
+
+
+@receiver(user_logged_in)
+def on_login(sender, user, request, **kwargs):
+    messages.success(request, f'{user.username} successfully logged in!')
+
+@receiver(user_logged_out)
+def on_logout(sender, user, request, **kwargs):
+    messages.success(request, f'{user.username} successfully logged out!')
